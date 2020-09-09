485d815cf598be2fe215e72f7505794826667560
Ole <olemartinhs@hotmail.com>
2020-09-09 10:46:20 +0200

    New test script

--- roaming/pos_test.py
+++ roaming/pos_test.py
@@ -4,7 +4,11 @@ import json
 f = open('heavy_lab_positions.json')
 data = json.load(f)
 
-for i in data:
-    print(i)
+for pos in data:
+    position = pos["pose"]["position"]
+    orientation = pos["pose"]["orientation"]
+
+    
+
 
 f.close()
--- roaming/roaming.py
+++ roaming/roaming.py
@@ -15,36 +15,51 @@ def movebase_client():
     f = open('heavy_lab_positions.json')
     data = json.load(f)
 
-    for i in data:
-        print(i)
-
-    f.close()
-   # Create an action client called "move_base" with action definition file "MoveBaseAction"
+    # Create an action client called "move_base" with action definition file "MoveBaseAction"
     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
  
    # Waits until the action server has started up and started listening for goals.
     client.wait_for_server()
 
-   # Creates a new goal with the MoveBaseGoal constructor
-    goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = "map"
-    goal.target_pose.header.stamp = rospy.Time.now()
-   # Move 0.5 meters forward along the x axis of the "map" coordinate frame 
-    goal.target_pose.pose.position.x = 0.5
-   # No rotation of the mobile base frame w.r.t. map frame
-    goal.target_pose.pose.orientation.w = 1.0
+    for pos in data:
 
-   # Sends the goal to the action server.
-    client.send_goal(goal)
-   # Waits for the server to finish performing the action.
-    wait = client.wait_for_result()
-   # If the result doesn't arrive, assume the Server is not available
-    if not wait:
-        rospy.logerr("Action server not available!")
-        rospy.signal_shutdown("Action server not available!")
-    else:
-    # Result of executing the action
-        return client.get_result()   
+        # Creates a new goal with the MoveBaseGoal constructor
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.header.stamp = rospy.Time.now()
+
+        position = pos["pose"]["position"]
+        orientation = pos["pose"]["orientation"]
+
+        goal.target_pose.pose.position.x = position["x"]
+        goal.target_pose.pose.position.y = position["y"]
+        goal.target_pose.pose.position.z = position["z"]
+
+        # Sends the goal to the action server.
+        client.send_goal(goal)
+        # Waits for the server to   finish performing the action.
+        wait = client.wait_for_result()
+
+        # If the result doesn't arrive, assume the Server is not available
+        if not wait:
+            rospy.logerr("Action server not available!")
+            rospy.signal_shutdown("Action server not available!")
+        else:
+        # Result of executing the action
+            return client.get_result()   
+
+
+    f.close()
+   
+
+   
+   # Move 0.5 meters forward along the x axis of the "map" coordinate frame 
+    #goal.target_pose.pose.position.x = 0.5
+   # No rotation of the mobile base frame w.r.t. map frame
+    #goal.target_pose.pose.orientation.w = 1.0
+
+   
+   
 
 # If the python node is executed as main process (sourced directly)
 if __name__ == '__main__':
